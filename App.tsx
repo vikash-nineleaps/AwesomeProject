d6ca0a5c691def2aa5635c7cc6af7a2339e65df8
Vikash <vikash.kumar@nineleaps.com>
2023-03-03 13:42:02 +0530

    second

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, View, Text, Image} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View style={{ justifyContent: 'center', alignItems: 'center'}}>
+        <Text>Hello World!!</Text>
+      </View>
+      
+    </SafeAreaView>
+  );
+};
+
+export default App;
